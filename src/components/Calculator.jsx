394a35103d4b6f9ca2d8c33a227b5f4932529e2d
Danny <Lorddanp@aol.com>
2018-12-16 18:05:11 -0500

    Calculator(Calculator): scaffolds Calculator component, the container for the app

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Calculator = props => {
+  return (
+    <div id="calculator-outer-div">
+      <div>screen placeholder</div>
+      <div>
+        buttons outer div placeholder
+        <div>buttons numerical grid div</div>
+        <div>buttons operations column div</div>
+      </div>
+    </div>
+  );
+};
