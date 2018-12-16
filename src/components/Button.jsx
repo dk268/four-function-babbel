984334e734038c413f368e72e2b41ba5e3625950
Danny <Lorddanp@aol.com>
2018-12-16 18:38:21 -0500

    Redux(Button): connects Button to redux

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const Button = props => {
   const { buttonText, buttonType, buttonValue } = props;
   return <div id={buttonText + "-button-div"}>{buttonText}</div>;
 };
 
-export default Button;
+const mapStateToProps = state => {};
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Button);
